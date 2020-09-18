bc5e64484c7ae10837e88dc5ea99840470eadf2b
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-09-18 13:39:13 +0800

    restart quiz

--- lib/main.dart
+++ lib/main.dart
@@ -51,8 +51,14 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
-  void _answerQuestion(int score) {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
+  void _answerQuestion(int score) {
     _totalScore += score;
 
     setState(() {
@@ -80,7 +86,7 @@ class _MyAppState extends State<MyApp> {
                 questionsIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText = "You are done!";
@@ -23,10 +24,19 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase + ' \nscore: ' +resultScore.toString(),
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase + ' \nscore: ' + resultScore.toString(),
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text("Restart Quiz!"),
+            textColor: Colors.blue,
+          )
+        ],
       ),
     );
   }
